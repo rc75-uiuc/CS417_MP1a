ec87a29ef8a5604a2354173c3ab58c37d29af4eb
rc75-uiuc <rc75@illinois.edu>
2026-09-11 17:54:20 -0500

    particles and audio support

--- Assets/spawnerScript.cs
+++ Assets/spawnerScript.cs
@@ -7,6 +7,9 @@ public class ObjectSpawner : MonoBehaviour
     public Transform spawnPoint;
     public float projectileSpeed = 5f;
 
+    public ParticleSystem spawnParticles;
+    public AudioClip spawnSound;
+
     private InputDevice rightController;
     private bool previousTriggerState = false;
 
@@ -26,6 +29,7 @@ public class ObjectSpawner : MonoBehaviour
             if (triggerPressed && !previousTriggerState)
             {
                 SpawnObject();
+                PlayEffects();
             }
 
             previousTriggerState = triggerPressed;
@@ -45,6 +49,24 @@ public class ObjectSpawner : MonoBehaviour
         if (projectile != null)
         {
             projectile.velocity = spawnPoint.forward * projectileSpeed;
+        }
+    }
+
+    void PlayEffects()
+    {
+        if (spawnParticles != null)
+        {
+            spawnParticles.transform.position = spawnPoint.position;
+            spawnParticles.transform.rotation = spawnPoint.rotation;
+            spawnParticles.Play();
+        }
+
+        if (spawnSound != null)
+        {
+            AudioSource.PlayClipAtPoint(
+                spawnSound,
+                spawnPoint.position
+            );
         }
     }
 }
